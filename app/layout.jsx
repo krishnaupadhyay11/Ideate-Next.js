cc00a57691637567954c4af5a785c734fd42b364
Krishna Upadhyay <krishna.upadhyay911@gmail.com>
2024-04-05 17:05:08 +0530

    keepgooo

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import '@styles/global.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: 'MPrompts',
+    description: 'HUSTLE for making jeevan sarthak'
+}
+
+const Layout = ({children}) => {
+  return (
+    <html lang='en'>
+        <head>
+            <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap" rel="stylesheet" />
+        </head>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+                <Nav />
+                
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default Layout;
---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Feed from "@components/Feed";
+
+function Home() {
+  return (
+    <section className='w-full flex-col flex-center'>
+      <h1 className='head_text text-center'>
+        Discover & Share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'>
+          AI Powered Prompts
+        </span>
+      </h1>
+      <p className='desc text-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui vero odit praesentium accusantium itaque, ipsum labore, incidunt, atque sint et quisquam!
+      </p>
+
+      <Feed />
+    </section>
+  )
+}
+
+export default Home;
